f990583b1ab64a9177087dab061119a4bf3d7f68
Cory Schmitt <cory@schmitty.me>
2018-04-09 08:59:57 -0700

    Initial commit

---
+++ lib/ex_taxjar.ex
@@ -0,0 +1,18 @@
+defmodule ExTaxjar do
+  @moduledoc """
+  Documentation for ExTaxjar.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTaxjar.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_taxjar_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTaxjarTest do
+  use ExUnit.Case
+  doctest ExTaxjar
+
+  test "greets the world" do
+    assert ExTaxjar.hello() == :world
+  end
+end
