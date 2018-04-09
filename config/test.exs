a4d2a482087976e2c67cef6c0a9a7dce166e9463
Cory Schmitt <cory@schmitty.me>
2018-04-09 13:13:41 -0700

    Remove api key from headers

--- config/test.exs
+++ config/test.exs
@@ -3,5 +3,6 @@ use Mix.Config
 config :exvcr, [
   filter_sensitive_data: [
     [pattern: "Bearer [0-9a-z]+", placeholder: "<<access_key>>" ]
-  ]
+  ],
+  response_headers_blacklist: ["Set-Cookie", "X-Request-Id", "Etag"]
 ]
